55e5f4c21f79dee855ba3b65fd6fd5decd8ad31e
Susan Wie <susanhwie@gmail.com>
2016-06-23 15:30:39 +0000

    added square function

--- command_line_calculator.rb
+++ command_line_calculator.rb
@@ -11,8 +11,10 @@ end
 
 puts "1 = Add          2 = Multiply"
 puts
-puts "3 = Subtract     4 = Divide\n "
-
+puts "3 = Subtract     4 = Divide"
+puts
+puts "5 = Square                 "
+puts
 puts "What operation do you want to use?"
 operation = gets.chomp.to_i
 
@@ -49,6 +51,16 @@ elsif operation == 4
   end
   puts "Your answer is #{results}!"
 
+elsif operation == 5
+  if values.length > 1
+    puts "invalid input"
+  end
+  if values.length == 1
+    puts "To what power?"
+    power = gets.chomp.to_i
+    results = values[0]**power
+    puts "Your answer is #{results}!"
+  end
 else
   puts "Invalid Input"
 end
